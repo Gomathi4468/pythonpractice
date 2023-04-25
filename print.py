65caca9cc1b1c8f6a501feed77b5f2d48e0345a1
Gomathi4468 <71780516+Gomathi4468@users.noreply.github.com>
2023-04-25 18:54:31 +0530

    print statement program

---
+++ print.py
@@ -0,0 +1,10 @@
+#print() function is used to print the statement in output screen
+''' welcome to
+ocean acadmey'''#multiline comment
+#\n-It is used to generate new line
+#end=""-It is used to not generate new line
+#\t-It is used to generate tab space
+print("Welcome to \nOcean \nAcademy",end=" ")
+print("Software Training Institute")
+print("\t\t\tOcean Academy")
+print("_________________________________________________")   
